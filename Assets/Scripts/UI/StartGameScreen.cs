54314017b22675516d87a759199e1b3bdecb963e
Erica Calaça <ericazpc@gmail.com>
2024-07-19 10:09:05 -0700

    Added a exit button on the start scene

--- Assets/Scripts/UI/StartGameScreen.cs
+++ Assets/Scripts/UI/StartGameScreen.cs
@@ -9,14 +9,21 @@ namespace ShootingGallery
     public class StartGameScreen : MonoBehaviour
     {
         [SerializeField] private Button _btnStart;
+        [SerializeField] private Button _btnExit;
         void Start()
         {
             _btnStart.onClick.AddListener(OnStartGame);
+            _btnExit.onClick.AddListener(OnExitGame);
         }
 
         private void OnStartGame()
         {
             SceneManager.LoadScene("MainScene");
+        }
+
+        private void OnExitGame()
+        {
+            Application.Quit();
         }
 
     }
